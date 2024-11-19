b877f7ced03d65162452e1f243e8904a64451e18
Kezziah Ayuno <kezziahayuno@kayuno.local>
2024-11-19 13:17:35 -0800

    updated landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,30 @@ import React from 'react';
 export default function Home() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px'}}>
-      <h1>Welcome to My Test</h1>
-      <p>Simplified text.</p>
+      <h1>earthtokezzi</h1>
+      <p>probably in space.</p>
+      <nav>
+        <ul>
+          <li>
+            <a href="/music">all things music</a>
+          </li>
+          <li>
+            <a href="/cooking">all things cooking</a>
+          </li>
+          <li>
+            <a href="/tech-writing">all things tech writing</a>
+          </li>
+          <li>
+            <a href="/league">all things league of legends</a>
+          </li>
+          <li>
+            <a href="/style">earthtokezzi style guides</a>
+          </li>
+          <li>
+            <a href="/about">all things about me</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
