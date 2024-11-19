f1c34ab15a3a50d97c64032959db2147f40af552
Kezziah Ayuno <kezziahayuno@kayuno.local>
2024-11-19 13:15:40 -0800

    made docs false in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ module.exports = {
     [
       '@docusaurus/preset-classic',
       {
+        docs: false,
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
