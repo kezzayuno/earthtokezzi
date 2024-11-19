d31670775651ecf124ef1c4aae496ea673a7443f
Kezziah Ayuno <kezziahayuno@kayuno.local>
2024-11-19 11:59:35 -0800

    push docu website

---
+++ docusaurus.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: 'earthtokezzi',
+  tagline: 'Test tagline',
+  url: 'https://earthtokezzi.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationname: 'kezzayuno',
+  projectName: 'earthtokezzi',
+  presets: [
+    '@docusaurus/preset-classic',
+    {
+      docs: {
+        sidebarPath: require.resolve('./sidebars.js'),
+        editUrl: 'https://github.com/kezzayuno/earthtokezzi/edit/main/',
+      },
+      theme: {
+        customCss: require.resolve('./src/css/custom.css'),
+      },
+    },
+  ],
+  themeConfig: {
+    navbar: {
+      title: 'earthtokezzi',
+      logo: {
+        alt: 'earthtokezzi logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'test',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/kezzayuno/earthtokezzi',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} earthtokezzi.`,
+    },
+  },
+};
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px'}}>
+      <h1>Welcome to My Test</h1>
+      <p>Simplified text.</p>
+    </div>
+  );
+}
